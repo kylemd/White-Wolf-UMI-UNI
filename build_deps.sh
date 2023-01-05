a5fe00679fa478fb87a6cc660bd81f2a38aa0d7c
kylemd <kyle.m.drayton@gmail.com>
2023-01-05 21:20:54 +1100

    Create build_deps.sh
    Had to ask which toolchains to use so adding a script to grab them to save time for others in the future

---
+++ build_deps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build system vars
+BUILD_USER=""
+BASE_DIR="/home/$BUILD_USER/AndroidBuilds"
+
+# Build dirs
+KERNELFLASHER_DIR="$BASE_DIR/Kernel_Flasher"
+
+# Toolchain paths
+TOOLCHAIN_DIR="$BASE_DIR/Toolchains"
+CLANG_DIR="$TOOLCHAIN_DIR/clang"
+GCC_DIR="$TOOLCHAIN_DIR/gcc"
+
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 $CLANG_DIR
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $GCC_DIR
+git clone https://github.com/Zackptg5/MMT-Extended.git $KERNELFLASHER_DIR
