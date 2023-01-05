ab02122cffeaa602e72ade477e670c122f71b32c
kylemd <kyle.m.drayton@gmail.com>
2023-01-05 21:49:13 +1100

    Clone Android 13 branch only

--- build_deps.sh
+++ build_deps.sh
@@ -12,6 +12,6 @@ TOOLCHAIN_DIR="$BASE_DIR/Toolchains"
 CLANG_DIR="$TOOLCHAIN_DIR/clang"
 GCC_DIR="$TOOLCHAIN_DIR/gcc"
 
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 $CLANG_DIR
-git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $GCC_DIR
-git clone https://github.com/Zackptg5/MMT-Extended.git $KERNELFLASHER_DIR
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android13-s3-release $CLANG_DIR
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $GCC_DIR
+git clone --depth=1 https://github.com/Zackptg5/MMT-Extended.git $KERNELFLASHER_DIR
